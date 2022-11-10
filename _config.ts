ccece6723e8519b20ce1fdf3147a8485d1210118
Oscar Otero <oom@oscarotero.com>
2022-11-10 18:53:20 +0100

    config

--- _config.ts
+++ _config.ts
@@ -1,8 +1,7 @@
 import lume from "lume/mod.ts";
+import basePath from "lume/plugins/base_path.ts";
 import blog from "https://deno.land/x/lume_theme_simple_blog@v0.2.0/mod.ts";
 
-const site = lume();
-
-site.use(blog());
-
-export default site;
+export default lume({ location: new URL("https://lume.com/blog/") })
+  .use(blog())
+  .use(basePath());
