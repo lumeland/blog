cc79cbed6419cd5a6e5f1782fa1681b1e55240cd
Oscar Otero <oom@oscarotero.com>
2022-11-16 19:38:29 +0100

    fixed url

--- _config.ts
+++ _config.ts
@@ -2,7 +2,7 @@ import lume from "lume/mod.ts";
 import basePath from "lume/plugins/base_path.ts";
 import blog from "https://deno.land/x/lume_theme_simple_blog@v0.3.0/mod.ts";
 
-export default lume({ location: new URL("https://lume.com/blog/") })
+export default lume({ location: new URL("https://lume.land/blog/") })
   .use(blog({
     prism: {
       languages: ["yaml", "markdown", "js", "ts", "css", "liquid"],
