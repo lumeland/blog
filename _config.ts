27bd08d2187f4126594148c340ed111b4b9e3273
Oscar Otero <oom@oscarotero.com>
2023-11-15 13:51:41 +0100

    updated deps

--- _config.ts
+++ _config.ts
@@ -1,6 +1,6 @@
 import lume from "lume/mod.ts";
 import basePath from "lume/plugins/base_path.ts";
-import blog from "https://deno.land/x/lume_theme_simple_blog@v0.12.1/mod.ts";
+import blog from "https://deno.land/x/lume_theme_simple_blog@v0.12.2/mod.ts";
 
 import "npm:prismjs/components/prism-markdown.js";
 import "npm:prismjs/components/prism-yaml.js";
@@ -8,7 +8,7 @@ import "npm:prismjs/components/prism-markup-templating.js";
 import "npm:prismjs/components/prism-liquid.js";
 import "npm:prismjs/components/prism-typescript.js";
 import "npm:prismjs/components/prism-json.js";
-import "https://deno.land/x/vento@v0.8.0/prism-vento.js";
+import "https://deno.land/x/vento@v0.9.1/prism-vento.js";
 
 export default lume({ location: new URL("https://lume.land/blog/") })
   .use(blog())
